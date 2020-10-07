2af298c5e964074eb87bf0a1e4635f94ad7c9ea7
avnish986 <avnishgupta986@gmail.com>
2020-10-07 09:16:43 +0530

    [Avnish]Added: uc1 board initailise

--- src/com/game/tictactoe/TicTacToe.java
+++ src/com/game/tictactoe/TicTacToe.java
@@ -1,9 +1,22 @@
 package com.game.tictactoe;
 
 public class TicTacToe {
-
+	static char[][] BOARD = new char[3][3];
+	static boolean notFull = true;
+	static void startBoard() {
+		System.out.println("TIC TAC TOE GAME\nComputer is O\nPlayer is X ");
+		for (int i = 0; i < BOARD.length; i++) {
+			for (int j = 0; j < BOARD[i].length; j++) {
+				BOARD[i][j] = ' ';
+			}
+		}
+		System.out.println("Board: ");
+		}
+	
+	
 	public static void main(String[] args) {
 		System.out.println("Welcome to TicTacToe");
+		startBoard();
 	}
 
 }
